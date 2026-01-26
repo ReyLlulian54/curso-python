99530da664153f430919996ac6d67679eb8d3531
ReyLlulian <julian.boltio54@gmail.com>
2026-01-26 16:04:06 -0300

    Ejercicios Basicos

--- 01_basic/exercises.py
+++ 01_basic/exercises.py
@@ -11,7 +11,7 @@ print("Escribe un programa que imprima tu nombre y tu ciudad en líneas separada
 
 ### Completa aquí
 
-print("--------------")
+print("Julian", "Santiago", sep=" ")
 
 print("\nEjercicio 2: Muestra los tipos de datos de las siguientes variables:")
 print("Usa el comando 'type()' para determinar el tipo de datos de cada variable.")
@@ -23,7 +23,12 @@ e = None
 
 ### Completa aquí
 
-print("--------------")
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
 
 print("\nEjercicio 3: Casting de tipos")
 print("Convierte la cadena \"12345\" a un entero y luego a un float.")
@@ -31,7 +36,10 @@ print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
 
 ### Completa aquí
 
-print("--------------")
+candena = int("12345")
+print(type(candena))
+print(type(float(candena)))
+print(int(3.99))
 
 print("\nEjercicio 4: Variables")
 print("Crea variables para tu nombre, edad y altura.")
@@ -40,11 +48,17 @@ print("Usa f-strings para imprimir una presentación.")
 # "Hola! Me llamo midudev y tengo 39 años, mido 1.70 metros"
 
 ### Completa aquí
-
-print("--------------")
+name = "Julian"
+age = 23
+altura = 1.83
+print(f"Hola! Me llamo {name} y tengo {age} años, mido {altura} metros")
 
 print("\nEjercicio 5: Números")
 print("1. Crea una variable con el número PI (sin asignar una variable)")
 print("2. Redondea el número con round()")
 print("3. Haz la división entera entre el número que te salió y el número 2")
 print("4. El resultado debería ser 1")
+
+a = 3.14159
+print(round(a))
+print(a // 2)
